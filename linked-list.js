c2c3504df3d1bedcfc4543245bea576dc4445431
Gretten <shhworkmail@yandex.ru>
2021-02-17 19:16:34 +0300

    some method improvements

--- linked-list.js
+++ linked-list.js
@@ -1,42 +1,109 @@
 class Node {
     constructor(data, prev, next) {
         this.data = data;
-        this.prev = prev;
-        this.next = next;
+        this.prev = prev || null;
+        this.next = next || null; 
     }
 }
 
 class LinkedList {
     constructor() {
         this.head = this.tail = null;
+        this.size = 0;
     }
 
-    append() {
-
+    append(data) {
+        if(!this.tail) {
+            this.tail = this.head = new Node(data)
+        } else {
+            let oldTail = this.tail;
+            this.tail = new Node(data);
+            oldTail.next = this.tail;
+            this.tail.prev = oldTail;
+        }
+        this.size++;
     }
 
-    prepend() {
-
+    prepend(data) {
+        if(!this.head) {
+            this.head = this.tail = new Node(data)
+        } else {
+            let oldHead = this.head;
+            this.head = new Node(data);
+            oldHead.prev = this.head;
+            this.head.next = oldHead;
+        }
+        this.size++;
     }
 
     deleteHead() {
-
+        if(!this.head) {
+            return;
+        }
+        else {
+            this.size--;
+            let removedHead = this.head;
+            if(this.head === this.tail) {
+                this.head = this.tail = null;
+            } else {
+                this.head = this.head.next;
+                this.head.prev = null;
+            }
+            return removedHead.value;
+        }
     }
 
-    search() {
+    deleteTail() {
+        if(!this.tail) {
+            return;
+        }
+        else {
+            this.size--;
+            let removedTail = this.tail;
+            if(this.tail === this.head) {
+                this.tail = this.head = null;
+            } else {
+                this.tail = this.tail.prev;
+                this.head.next = null;
+            }
+            return removedTail.value;
+        }
+    }
+    search(value) {
+        let current = this.head;
+        while(current) {
+            if(value === current.data) return true;
+            current = current.next;
+        }
+    }
 
+    getSize() {
+        return this.size;
     }
 
     printData() {
 
     }
 
-    addToIndex() {
+    addToIndex(data, index) {
 
     }
 
-    delFromIndex() {
+    deleteFromIndex(data, index) {
 
     }
 
 }
+
+const ll = new LinkedList();
+
+ll.append(50);
+ll.append(180);
+ll.append(2780);
+ll.append(4452780);
+console.log(ll.getSize())
+
+console.log(ll.search(4452780))
+
+
+console.log(ll)
