ebad4ebad19f6d18b11b764fd60907ad66b0c5e8
Gretten <shhworkmail@yandex.ru>
2021-02-17 17:04:51 +0300

    init

---
+++ linked-list.js
@@ -0,0 +1,42 @@
+class Node {
+    constructor(data, prev, next) {
+        this.data = data;
+        this.prev = prev;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = this.tail = null;
+    }
+
+    append() {
+
+    }
+
+    prepend() {
+
+    }
+
+    deleteHead() {
+
+    }
+
+    search() {
+
+    }
+
+    printData() {
+
+    }
+
+    addToIndex() {
+
+    }
+
+    delFromIndex() {
+
+    }
+
+}
